a81805541d7bdd6c33ce2d870b1ea23a0f09b7da
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-06-07 16:56:24 -0300

    Projeto de referência

---
+++ Java/Desenvolvimento Java com IA/exercicios/main/desenvolvimento-java-projeto-referencia/src/main/java/me/dio/controller/UserController.java
@@ -0,0 +1,47 @@
+package me.dio.controller;
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        try {
+            var user = userService.findById(id);
+            return ResponseEntity.ok(user);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
---
+++ Java/Desenvolvimento Java com IA/exercicios/main/desenvolvimento-java-projeto-referencia/src/test/java/me/dio/desenvolvimento_java/DesenvolvimentoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desenvolvimento_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvimentoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
