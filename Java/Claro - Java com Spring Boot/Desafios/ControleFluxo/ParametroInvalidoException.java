a31ece332b8d778462a2f043a401fa78a2855fcb
Huugo <huugoalexanndre@gmail.com>
2024-09-12 15:44:14 -0300

    Desafio 2 - Contador

---
+++ Java/Claro - Java com Spring Boot/Desafios/ControleFluxo/ParametroInvalidoException.java
@@ -0,0 +1,7 @@
+package ControleFluxo;
+
+public class ParametroInvalidoException extends Exception {
+    public ParametroInvalidoException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
